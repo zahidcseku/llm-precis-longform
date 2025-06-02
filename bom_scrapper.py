790ad97e5b4c2cef8f3d8a61002a152a6245edcd
Zahid Islam <zahidfyibiz@gmail.com>
2025-06-02 10:29:48 +1000

    fixed CST AST issues

--- bom_scrapper.py
+++ bom_scrapper.py
@@ -67,23 +67,30 @@ def scrape_forecast_texts(state, city):
 
         # replace "Forecast for the rest of " by date and day from "issued_at"
         # check if it has ESTon
-        if "ESTon" in forecast_data["issued_at"]:
+        check_split_keys = forecast_data["issued_at"].split()
+
+        if "ESTon" in check_split_keys:
             # split_key = "ESTon"
             date_delimiter_pattern = r"\bESTon\b"
-        elif "on" in forecast_data["issued_at"]:
+        elif "CSTon" in check_split_keys:
+            # split_key = "ESTon"
+            date_delimiter_pattern = r"\bCSTon\b"
+        elif "on" in check_split_keys:
             # split_key = "on"
             date_delimiter_pattern = r"\bon\b"
         else:
-            print("No split_key found in issued_at")
-            print(forecast_data["issued_at"])
+            print(f"No split_key found in issued_at: {forecast_data['issued_at']}")
+            print(check_split_keys)
             exit()
 
+        # print(forecast_data["issued_at"])
+        # print(date_delimiter_pattern)
         # date_to_update = forecast_data["issued_at"].split(split_key)[1]
         date_to_update = re.split(
             date_delimiter_pattern, forecast_data["issued_at"], 1
         )[1]
         date_to_update = date_to_update[:-5]
-        # print(forecast_data["issued_at"])
+
         # print(re.split(date_delimiter_pattern, forecast_data["issued_at"], 1))
         # print(date_to_update)
         # exit()
